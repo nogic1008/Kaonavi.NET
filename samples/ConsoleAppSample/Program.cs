b93205805eb76dcd5c22cd5dd910804f2b89f012
nogic <24802730+nogic1008@users.noreply.github.com>
2022-01-04 14:56:58 +0900

    refactor(sample): use minimal API

--- samples/ConsoleAppSample/Program.cs
+++ samples/ConsoleAppSample/Program.cs
@@ -1,27 +1,48 @@
-using ConsoleAppFramework;
 using ConsoleAppSample;
 using Kaonavi.Net.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-await Host.CreateDefaultBuilder()
-    .ConfigureLogging(logging => logging.ReplaceToSimpleConsole())
-    .ConfigureServices((context, services) =>
-    {
-        // IOptions
-        var config = context.Configuration;
-        services.Configure<KaonaviOptions>(config.GetSection(nameof(KaonaviOptions)));
+var builder = ConsoleApp.CreateBuilder(args);
+builder.ConfigureServices((ctx, services) =>
+{
+    // IOptions
+    var config = ctx.Configuration;
+    services.Configure<KaonaviOptions>(config.GetSection(nameof(KaonaviOptions)));
 
-        // DI
-        services.AddHttpClient<IKaonaviService, KaonaviV2Service>((client, provider) =>
+    // DI
+    services.AddHttpClient<IKaonaviService, KaonaviV2Service>((client, provider) =>
+    {
+        var options = provider.GetRequiredService<IOptions<KaonaviOptions>>().Value;
+        return new(client, options.ConsumerKey, options.ConsumerSecret)
         {
-            var options = provider.GetRequiredService<IOptions<KaonaviOptions>>().Value;
-            return new(client, options.ConsumerKey, options.ConsumerSecret)
-            {
-                UseDryRun = options.UseDryRun
-            };
-        });
-    })
-    .RunConsoleAppFrameworkAsync<AppBase>(args)
-    .ConfigureAwait(false);
+            UseDryRun = options.UseDryRun
+        };
+    });
+});
+var app = builder.Build();
+
+app.AddCommand("layout", "メンバー情報のレイアウトを取得します。", async (ConsoleAppContext ctx, IKaonaviService service) =>
+{
+    var memberLayout = await service.FetchMemberLayoutAsync(ctx.CancellationToken).ConfigureAwait(false);
+    ctx.Logger.LogInformation("Received Layout: {memberLayout}", memberLayout);
+});
+app.AddCommand("download", "メンバー情報を全取得します。", async (ConsoleAppContext ctx, IKaonaviService service) =>
+{
+    var employees = (await service.FetchMembersDataAsync(ctx.CancellationToken).ConfigureAwait(false))
+        .Select(m => new EmployeeData(m));
+    ctx.Logger.LogInformation("Received Data: {employees}", employees);
+});
+app.AddCommand("upload", "メンバー情報を更新します。", async (ConsoleAppContext ctx, IKaonaviService service) =>
+{
+    var employees = Enumerable.Range(1, 9)
+        .Select(i => new EmployeeData($"100{i}", $"User {i}", $"User {i}", $"{i}000", $"100{i}@example.com", "男", new(1990, 1, 1), "A", new(2012, 4, 1)));
+    int taskId = await service.UpdateMemberDataAsync(employees.Select(e => e.ToMemberData()).ToArray(), ctx.CancellationToken).ConfigureAwait(false);
+    ctx.Logger.LogInformation("Start task at (TaskId: {taskId})", taskId);
+});
+app.AddCommand("progress", "タスクの進捗状況を取得します。", async (ConsoleAppContext ctx, IKaonaviService service, [Option("t", "タスクID")] int taskId) =>
+{
+    var progress = await service.FetchTaskProgressAsync(taskId, ctx.CancellationToken).ConfigureAwait(false);
+    ctx.Logger.LogInformation("Received Progress: {progress}", progress);
+});
