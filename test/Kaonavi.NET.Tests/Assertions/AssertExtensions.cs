9259f0b5138b7608d6db341e7cfe9ae8cc0d6143
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2024-11-17 14:55:22 +0900

    style: merge extensions

--- test/Kaonavi.NET.Tests/Assertions/AssertExtensions.cs
+++ test/Kaonavi.NET.Tests/Assertions/AssertExtensions.cs
@@ -1,11 +1,7 @@
 namespace Kaonavi.Net.Tests.Assertions;
 
-public static class HttpRequestMessageExtensions
+internal static class AssertExtensions
 {
     public static HttpRequestMessageAssertions Should(this HttpRequestMessage instance) => new(instance);
-}
-
-public static class JsonElementExtensions
-{
     public static JsonElementAssertions Should(this JsonElement instance) => new(instance);
 }
