58689e6d6193f7f8223e53715be4b8805177c4bb
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2024-11-17 16:13:45 +0900

    feat(entity): add CustomFieldLayout.TypeDetail property

---
+++ src/Kaonavi.NET.Core/Entities/FieldInput.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaonavi.Net.Entities;
+
+/// <summary>シート項目種別</summary>
+[JsonConverter(typeof(JsonStringEnumConverter<FieldInput>))]
+public enum FieldInput
+{
+    /// <summary>テキストボックス</summary>
+    [JsonStringEnumMemberName("text_box")] TextBox,
+    /// <summary>テキストエリア</summary>
+    [JsonStringEnumMemberName("text_area")] TextArea,
+    /// <summary>ナンバーボックス</summary>
+    [JsonStringEnumMemberName("number_box")] NumberBox,
+    /// <summary>プルダウンリスト</summary>
+    [JsonStringEnumMemberName("pull_down")] PullDown,
+    /// <summary>ラジオボタン</summary>
+    [JsonStringEnumMemberName("radio_button")] RadioButton,
+    /// <summary>チェックボックス</summary>
+    [JsonStringEnumMemberName("check_box")] CheckBox,
+    /// <summary>リンク</summary>
+    [JsonStringEnumMemberName("link")] Link,
+    /// <summary>日付（カレンダー）</summary>
+    [JsonStringEnumMemberName("date")] Date,
+    /// <summary>年月（カレンダー）</summary>
+    [JsonStringEnumMemberName("year_month")] YearMonth,
+    /// <summary>ファイル添付</summary>
+    [JsonStringEnumMemberName("attach_file")] AttachFile,
+    /// <summary>顔写真</summary>
+    [JsonStringEnumMemberName("face_image")] FaceImage,
+    /// <summary>計算式</summary>
+    [JsonStringEnumMemberName("calc_formula")] CalcFormula,
+}
