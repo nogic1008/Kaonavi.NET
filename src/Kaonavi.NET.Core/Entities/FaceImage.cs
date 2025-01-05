d3a34731b9814c05d2feda481a3d91a28cde5a24
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2025-01-05 16:20:46 +0900

    feat(entities): add FaceImage

---
+++ src/Kaonavi.NET.Core/Entities/FaceImage.cs
@@ -0,0 +1,8 @@
+namespace Kaonavi.Net.Entities;
+
+/// <summary>
+/// メンバー情報 顔写真
+/// </summary>
+/// <param name="Code">社員番号</param>
+/// <param name="Content">顔写真画像(5MBまで)</param>
+public record FaceImage(string Code, [property: JsonPropertyName("base64_face_image")] byte[] Content);
