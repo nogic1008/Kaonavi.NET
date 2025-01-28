833463efa4d2682d4f28a45890b3c1909136868f
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2025-01-28 21:51:46 +0900

    perf(json): use `Span<byte>` on `DateTimeConverter.Write`

--- src/Kaonavi.NET.Core/Json/DateTimeConverter.cs
+++ src/Kaonavi.NET.Core/Json/DateTimeConverter.cs
@@ -12,5 +12,11 @@ public class DateTimeConverter : JsonConverter<DateTime>
 
     /// <inheritdoc/>
     public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
-        => writer.WriteStringValue(value.ToString(Format, CultureInfo.InvariantCulture));
+    {
+        Span<byte> buffer = stackalloc byte[21]; // "yyyy-MM-dd HH:mm:ss".Length
+        buffer[0] = (byte)'"';
+        _ = value.TryFormat(buffer[1..], out int written, Format, CultureInfo.InvariantCulture);
+        buffer[written + 1] = (byte)'"';
+        writer.WriteRawValue(buffer[0..(written + 2)], false);
+    }
 }
