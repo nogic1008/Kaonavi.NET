0fb9ab2228bf704b27ebac6db5d29518b01d02ba
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2025-05-18 19:26:52 +0900

    test(generator): split e2e test

--- test/Kaonavi.NET.Generator.Tests/SheetDataGenerator.Test.cs
+++ test/Kaonavi.NET.Generator.Tests/SheetDataGenerator.Test.cs
@@ -1,8 +1,5 @@
-using System.Globalization;
 using System.Text;
-using Kaonavi.Net.Generator.Tests.Entities;
 using Microsoft.CodeAnalysis.CSharp;
-using RandomFixtureKit;
 
 namespace Kaonavi.Net.Generator.Tests;
 
@@ -11,7 +8,7 @@ namespace Kaonavi.Net.Generator.Tests;
 public sealed class SheetDataGeneratorTest
 {
     /// <summary>
-    /// <see cref="When_Invalid_Code_Compiler_Warns_With_Diagnostic(string, string)"/>用のテストデータ
+    /// <see cref="When_Invalid_Code_Compiler_Warns_With_Diagnostic"/>用のテストデータ
     /// </summary>
     public static IEnumerable<TestDataRow<(string code, string id)>> InvalidCodeTestData
     {
@@ -106,7 +103,7 @@ public sealed class SheetDataGeneratorTest
     /// <summary>
     /// <paramref name="code"/>をコンパイルした場合、<paramref name="id"/>のコンパイル警告が発生する。
     /// </summary>
-    /// <param name="code">ソースコード</param>
+    /// <param name="code">生成元となるソースコード</param>
     /// <param name="id">コンパイル警告のID</param>
     [TestMethod("Generator > コンパイル警告が発生する。")]
     [DynamicData(nameof(InvalidCodeTestData))]
@@ -119,6 +116,9 @@ public sealed class SheetDataGeneratorTest
         warnings.ShouldContain(d => d.Id == id);
     }
 
+    /// <summary>
+    /// <see cref="Generates_ToCustomFields_Method"/>用のテストデータ
+    /// </summary>
     public static IEnumerable<TestDataRow<(LanguageVersion version, string code, string expected)>> ValidCodeTestData
     {
         get
@@ -257,7 +257,7 @@ public sealed class SheetDataGeneratorTest
     /// <paramref name="version"/>を指定して<paramref name="code"/>をコンパイルした場合、<paramref name="expected"/>と同一のコードが生成される。
     /// </summary>
     /// <param name="version">C# のバージョン</param>
-    /// <param name="code">ソースコード</param>
+    /// <param name="code">生成元となるソースコード</param>
     /// <param name="expected">生成されるソースコード</param>
     [TestMethod]
     [DynamicData(nameof(ValidCodeTestData))]
@@ -276,101 +276,5 @@ public sealed class SheetDataGeneratorTest
         // Assert
         warnings.ShouldBeEmpty();
         sb.ToString().ShouldBe(expected.Replace("\n", Environment.NewLine));
-    }
-
-    /// <summary>
-    /// <see cref="NormalClassSheetData"/>の<see cref="ISheetData.ToCustomFields"/>メソッドがソース生成される。
-    /// </summary>
-    [TestMethod($"Generator > [{nameof(SheetSerializableAttribute)}]属性を持つクラスがある場合、{nameof(ISheetData.ToCustomFields)}()をソース生成する。")]
-    public void Class_Generates_ToCustomFields_Method()
-    {
-        // Arrange
-        var values = FixtureFactory.CreateMany<NormalClassSheetData>(10);
-
-        // Act - Assert
-        foreach (var sut in values)
-        {
-            sut.ToCustomFields().ShouldBe([
-                new(101, sut.Name!),
-                new(102, sut.Date1.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(103, sut.Date2.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(104, sut.Date3.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(105, sut.Date4.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(106, sut.Date5.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(107, sut.Date6.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-            ]);
-        }
-    }
-
-    /// <summary>
-    /// <see cref="NormalRecordSheetData"/>の<see cref="ISheetData.ToCustomFields"/>メソッドがソース生成される。
-    /// </summary>
-    [TestMethod($"Generator > [{nameof(SheetSerializableAttribute)}]属性を持つ record クラスがある場合、{nameof(ISheetData.ToCustomFields)}()をソース生成する。")]
-    public void Record_Generates_ToCustomFields_Method()
-    {
-        // Arrange
-        var values = FixtureFactory.CreateMany<NormalRecordSheetData>(10);
-
-        // Act - Assert
-        foreach (var sut in values)
-        {
-            sut.ToCustomFields().ShouldBe([
-                new(101, sut.Name!),
-                new(102, sut.Date1.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(103, sut.Date2.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(104, sut.Date3.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(105, sut.Date4.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(106, sut.Date5.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(107, sut.Date6.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture))
-            ]);
-        }
-    }
-
-    /// <summary>
-    /// <see cref="NoNamespaceClassSheetData"/>の<see cref="ISheetData.ToCustomFields"/>メソッドがソース生成される。
-    /// </summary>
-    [TestMethod($"Generator > [{nameof(SheetSerializableAttribute)}]属性を持つ名前空間を持たないクラスがある場合、{nameof(ISheetData.ToCustomFields)}()をソース生成する。")]
-    public void Class_Without_Namespace_Generates_ToCustomFields_Method()
-    {
-        // Arrange
-        var values = FixtureFactory.CreateMany<NoNamespaceClassSheetData>(10);
-
-        // Act - Assert
-        foreach (var sut in values)
-        {
-            sut.ToCustomFields().ShouldBe([
-                new(101, sut.Name),
-                new(102, sut.Date1.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(103, sut.Date2.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(104, sut.Date3.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(105, sut.Date4.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(106, sut.Date5.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(107, sut.Date6.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture))
-            ]);
-        }
-    }
-
-    /// <summary>
-    /// <see cref="NoNamespaceRecordSheetData"/>の<see cref="ISheetData.ToCustomFields"/>メソッドがソース生成される。
-    /// </summary>
-    [TestMethod($"Generator > [{nameof(SheetSerializableAttribute)}]属性を持つ名前空間を持たない record クラスがある場合、{nameof(ISheetData.ToCustomFields)}()をソース生成する。")]
-    public void Record_Without_Namespace_Generates_ToCustomFields_Method()
-    {
-        // Arrange
-        var values = FixtureFactory.CreateMany<NoNamespaceRecordSheetData>(10);
-
-        // Act - Assert
-        foreach (var sut in values)
-        {
-            sut.ToCustomFields().ShouldBe([
-                new(101, sut.Name),
-                new(102, sut.Date1.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(103, sut.Date2.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(104, sut.Date3.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(105, sut.Date4.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(106, sut.Date5.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
-                new(107, sut.Date6.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture))
-            ]);
-        }
     }
 }
---
+++ test/Kaonavi.NET.Generator.Tests/ToCustomField.Test.cs
@@ -0,0 +1,106 @@
+using System.Globalization;
+using Kaonavi.Net.Generator.Tests.Entities;
+using RandomFixtureKit;
+
+namespace Kaonavi.Net.Generator.Tests;
+
+/// <summary><see cref="SheetDataGenerator"/>で生成される<see cref="ISheetData.ToCustomFields"/>メソッドのテスト</summary>
+[TestClass, TestCategory("Source Generator")]
+public sealed class ToCustomFieldTest
+{
+    /// <summary>
+    /// <see cref="NormalClassSheetData"/>の<see cref="ISheetData.ToCustomFields"/>メソッドがソース生成される。
+    /// </summary>
+    [TestMethod($"Generator > [{nameof(SheetSerializableAttribute)}]属性を持つクラスがある場合、{nameof(ISheetData.ToCustomFields)}()をソース生成する。")]
+    public void Class_Generates_ToCustomFields_Method()
+    {
+        // Arrange
+        var values = FixtureFactory.CreateMany<NormalClassSheetData>(10);
+
+        // Act - Assert
+        foreach (var sut in values)
+        {
+            sut.ToCustomFields().ShouldBe([
+                new(101, sut.Name!),
+                new(102, sut.Date1.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(103, sut.Date2.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(104, sut.Date3.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(105, sut.Date4.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(106, sut.Date5.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(107, sut.Date6.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+            ]);
+        }
+    }
+
+    /// <summary>
+    /// <see cref="NormalRecordSheetData"/>の<see cref="ISheetData.ToCustomFields"/>メソッドがソース生成される。
+    /// </summary>
+    [TestMethod($"Generator > [{nameof(SheetSerializableAttribute)}]属性を持つ record クラスがある場合、{nameof(ISheetData.ToCustomFields)}()をソース生成する。")]
+    public void Record_Generates_ToCustomFields_Method()
+    {
+        // Arrange
+        var values = FixtureFactory.CreateMany<NormalRecordSheetData>(10);
+
+        // Act - Assert
+        foreach (var sut in values)
+        {
+            sut.ToCustomFields().ShouldBe([
+                new(101, sut.Name!),
+                new(102, sut.Date1.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(103, sut.Date2.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(104, sut.Date3.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(105, sut.Date4.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(106, sut.Date5.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(107, sut.Date6.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture))
+            ]);
+        }
+    }
+
+    /// <summary>
+    /// <see cref="NoNamespaceClassSheetData"/>の<see cref="ISheetData.ToCustomFields"/>メソッドがソース生成される。
+    /// </summary>
+    [TestMethod($"Generator > [{nameof(SheetSerializableAttribute)}]属性を持つ名前空間を持たないクラスがある場合、{nameof(ISheetData.ToCustomFields)}()をソース生成する。")]
+    public void Class_Without_Namespace_Generates_ToCustomFields_Method()
+    {
+        // Arrange
+        var values = FixtureFactory.CreateMany<NoNamespaceClassSheetData>(10);
+
+        // Act - Assert
+        foreach (var sut in values)
+        {
+            sut.ToCustomFields().ShouldBe([
+                new(101, sut.Name),
+                new(102, sut.Date1.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(103, sut.Date2.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(104, sut.Date3.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(105, sut.Date4.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(106, sut.Date5.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(107, sut.Date6.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture))
+            ]);
+        }
+    }
+
+    /// <summary>
+    /// <see cref="NoNamespaceRecordSheetData"/>の<see cref="ISheetData.ToCustomFields"/>メソッドがソース生成される。
+    /// </summary>
+    [TestMethod($"Generator > [{nameof(SheetSerializableAttribute)}]属性を持つ名前空間を持たない record クラスがある場合、{nameof(ISheetData.ToCustomFields)}()をソース生成する。")]
+    public void Record_Without_Namespace_Generates_ToCustomFields_Method()
+    {
+        // Arrange
+        var values = FixtureFactory.CreateMany<NoNamespaceRecordSheetData>(10);
+
+        // Act - Assert
+        foreach (var sut in values)
+        {
+            sut.ToCustomFields().ShouldBe([
+                new(101, sut.Name),
+                new(102, sut.Date1.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(103, sut.Date2.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(104, sut.Date3.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(105, sut.Date4.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(106, sut.Date5.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+                new(107, sut.Date6.GetValueOrDefault().ToString("yyyy-MM-dd", CultureInfo.InvariantCulture))
+            ]);
+        }
+    }
+}
